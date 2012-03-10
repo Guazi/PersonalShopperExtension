dcd1617b88dea92afed738fa0e9083064b76b7b9
Dan Oved <stangogh@gmail.com>
2012-03-09 22:59:56 -0500

    Background page successfully prompts for username and saves it in local storage.

--- PersonalShopper.Extension/ShoppingListPage/ShoppingListPage.js
+++ PersonalShopper.Extension/ShoppingListPage/ShoppingListPage.js
@@ -4,15 +4,18 @@ PERSONALSHOPPER.BACKGROUNDPAGES = PERSONALSHOPPER.BACKGROUNDPAGES  || {};
 var debug = debug || PERSONALSHOPPER.UTILITIES.debug;
 
 PERSONALSHOPPER.BACKGROUNDPAGES.viewTemplates = {
-    getUserNameView : "<label for='userName'>Enter User Name</label><input type='text/javascript' value='{{userName}}' />",
-    loggedInUserNameView : "Your user name is {{userName}}",
+    getUserNameView : "<form class='getUserNameForm'>" +
+        "<label for='userName'>Enter User Name</label><input type='text' name='userName' value='{{userName}}' />" +
+        "<input type='submit' value='save''/>" +
+        "</form>",
+    loggedInUserNameView : "Your user name is {{userName}}. <a href=''#' class='setUserLink'>Set username</a>",
     notLoggedInUserNameView : "You don't have a username. <a href=''#' class='setUserLink'>Set username</a>"
 };
 
-PERSONALSHOPPER.BACKGROUNDPAGES.shoppingListController = (function($, viewTemplates, viewEngine, shoppingCartContextService){
+PERSONALSHOPPER.BACKGROUNDPAGES.ShoppingListController = (function($, viewTemplates, viewEngine, shoppingCartContextService){
     var MODELS = {
         userInformation : function(userName){
-            this.userName = userName
+            this.userName = userName;
         }
     };
     var Constr = function($view){
@@ -22,16 +25,18 @@ PERSONALSHOPPER.BACKGROUNDPAGES.shoppingListController = (function($, viewTempla
         renderUserInformation : function(){
             debug.log(['view is', this.$view]);
             var currentUserName = shoppingCartContextService.getLoggedInUserName();
-            var viewTemplate = currentUserName != null ? viewTemplates.loggedInUserNameView : viewTemplates.notLoggedInUserNameView;
-            debug.log(['template is', viewTemplate]);
-            var model = new MODELS.userInformation(currentUserName);
+            this.renderUserInformationView(currentUserName);
+        },
+        renderUserInformationView : function(userName){
+            var viewTemplate = userName != null ? viewTemplates.loggedInUserNameView : viewTemplates.notLoggedInUserNameView;
+            var model = new MODELS.userInformation(userName);
             var toPopulate = this.$view.find('fieldset.userInformation');
-            debug.log(['to populate', toPopulate]);
             viewEngine.renderInElement(viewTemplate, model, toPopulate);
 
             var self = this;
-            this.$view.find('a.setUserLink').click(function(){
+            this.$view.find('a.setUserLink').click(function(event){
                 self.promptForUserName.call(self);
+                event.preventDefault();
             });
         },
         promptForUserName : function(){
@@ -39,8 +44,19 @@ PERSONALSHOPPER.BACKGROUNDPAGES.shoppingListController = (function($, viewTempla
             var model = new MODELS.userInformation(currentUserName);
             var viewTemplate = viewTemplates.getUserNameView;
             var toPopulate = this.$view.find('fieldset.userInformation');
-
             viewEngine.renderInElement(viewTemplate, model, toPopulate);
+            var self = this;
+            this.$view.find('form.getUserNameForm').submit(function(event){
+                var form = this;
+                // todo: $ shortcut not working
+                userName = jQuery(form['userName']).val();
+                self.setUserName.call(self, userName);
+                event.preventDefault();
+            });
+        },
+        setUserName : function(userName){
+            shoppingCartContextService.setLoggedInUserName(userName);
+            return this.renderUserInformationView(userName);
         }
     };
     return Constr;
